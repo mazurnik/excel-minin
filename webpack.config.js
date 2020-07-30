83a77e3b78dd657f38cf4d05d87ba784d398c8ee
Mikalai <mazurnikv@gmail.com>
2020-07-30 07:45:30 +0300

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // подключили через дестриктуризацию
+const {
+  CleanWebpackPlugin
+} = require("clean-webpack-plugin"); // подключили через дестриктуризацию
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const isProd = process.env.NODE_ENV === "production"; // переменные для определения режима разроботки прод или дев
+const isDev = !isProd;
+
+const filename = ext => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
+
+const jsLoaders = () => {
+  const loaders = [{
+    loader: "babel-loader",
+    options: {
+      presets: ["@babel/preset-env"],
+    },
+  }];
+  if (isDev) {
+    loaders.push("eslint-loader");
+  }
+};
+
 module.exports = {
-  context: path.resolve(__dirname, "src"), //отвечает за то где лежат исходники
+  context: path.resolve(__dirname, "src"), // отвечает за то где лежат исходники
   mode: "development",
-  entry: "./index.js",
+  entry: ["@babel/polyfill", "./index.js"],
   output: {
-    filename: "bundle.[hash].js",
+    filename: filename((ext = "js")),
     path: path.resolve(__dirname, "dist"),
   },
   resolve: {
@@ -19,38 +38,50 @@ module.exports = {
       "@core": path.resolve(__dirname, "src/core"),
     },
   },
+  devtool: isDev ? "source-map" : false,
+  devServer: {
+    port: 3000,
+    hot: isDev,
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "index.html",
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
     }),
     new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "src/favicon.ico"),
-          to: path.resolve(__dirname, "dist"),
-        },
-      ],
+      patterns: [{
+        from: path.resolve(__dirname, "src/favicon.ico"),
+        to: path.resolve(__dirname, "dist"),
+      }],
     }),
     new MiniCssExtractPlugin({
-      filename: "bundle.[hash].css",
+      filename: filename((ext = "css")),
     }),
   ],
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [{
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
       },
 
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
+        use: {
+          jsLoaders,
         },
       },
     ],
