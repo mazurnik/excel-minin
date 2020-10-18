138b06307d9307dc6d958c974a60c679a6efe658
Mikalai <mazurnikv@gmail.com>
2020-10-18 21:27:29 +0300

    работаем над свои аналогом jQuery

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,9 +1,34 @@
 class Dom {
-
+    constructor(selector) {
+        this.$el = typeof selector === 'string' ? document.querySelector(selector) : selector
+    }
+    html(html) {
+        if (typeof html === 'string') {
+            this.$el.innerHTML = html
+            return this
+        }
+        return this.$el.outerHTML.trim()
+    }
+    clear() {
+        this.html('')
+        return this
+    }
+    append(node) {
+        if (node instanceof Dom) {
+            node = node.$el
+        }
+        if (Element.prototype.append) {
+            this.$el.append(node)
+        } else {
+            this.$el.appendChild(node)
+        }
+        return this
+    }
 }
+$('div').html('<h1>Test</h1>').clear()
 
-export function $() {
-    return new Dom()
+export function $(selector) {
+    return new Dom(selector)
 }
 
 $.create = (tagName, classes = '') => {
@@ -11,5 +36,5 @@ $.create = (tagName, classes = '') => {
     if (classes) {
         el.classList.add(classes)
     }
-    return el
+    return $(el)
 }
