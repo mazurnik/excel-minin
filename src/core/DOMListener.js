16738dcd2a12ad27917a709daf5a8ac7c2122098
Mikalai <mazurnikv@gmail.com>
2020-10-19 23:08:21 +0300

    создаем структуру для слушателей

--- src/core/DOMListener.js
+++ src/core/DOMListener.js
@@ -1,4 +1,14 @@
 /* eslint-disable require-jsdoc */
 export class DOMListener {
+    constructor($root, listeners = []) {
+        this.$root = $root
+        this.listeners = listeners
+    }
 
+    initDOMListeners() {
+console.log(this.listeners)
+    }
+
+    removeDOMListeners() {
+    }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,7 +1,14 @@
 import {DOMListener} from "./DOMListener";
 
 export class ExcelComponent extends DOMListener {
+constructor($root, options = {}) {
+        super($root, options.listeners)
+    }
     toHTML() {
         return ''
+    }
+
+    init() {
+        this.initDOMListeners()
     }
 }
